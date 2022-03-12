574dc655d3051ecaf915d4250d856cbb49992a5d
Jaderha <jaderhericks@gmail.com>
2022-03-12 11:24:14 -0300

    Atualizado títulos de alunos e criado cad professor

--- app/src/main/java/com/example/cadastroalunos/CadastroProfessorActivity.java
+++ app/src/main/java/com/example/cadastroalunos/CadastroProfessorActivity.java
@@ -1,15 +1,57 @@
 package com.example.cadastroalunos;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.example.cadastroalunos.util.CpfMask;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class CadastroProfessorActivity extends AppCompatActivity {
+
+    private TextInputEditText edNomeProfessor;
+    private TextInputEditText edCpfProfessor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle(R.string.ProfTitle);
         setContentView(R.layout.activity_cadastro_professor);
+
+        loadComponents();
+
+        edCpfProfessor.addTextChangedListener(CpfMask.insert(edCpfProfessor));
+    }
+
+    private void loadComponents() {
+        edNomeProfessor = findViewById(R.id.edNomeProfessor);
+        edCpfProfessor = findViewById(R.id.edCpfProfessor);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflaterMenu = getMenuInflater();
+        inflaterMenu.inflate(R.menu.menu_cadastro, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.mn_limpar:
+                //TODO: adicionar método  de limpar dados
+                //limparCampos();
+                return true;
+            case R.id.mn_salvar:
+                //TODO: adicionar método  de salvar dados
+                //validaCampos();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/com/example/cadastroalunos/model/Professor.java
+++ app/src/main/java/com/example/cadastroalunos/model/Professor.java
@@ -1,30 +1,21 @@
 package com.example.cadastroalunos.model;
 
 import com.orm.SugarRecord;
+import com.orm.dsl.Unique;
 
 import java.util.Objects;
 
 public class Professor extends SugarRecord {
 
-    private int idProfessor;
     private String nome;
     private String cpf;
 
     public Professor() {
     }
 
-    public Professor(int idProfessor, String nome, String cpf) {
-        this.idProfessor = idProfessor;
+    public Professor(String nome, String cpf) {
         this.nome = nome;
         this.cpf = cpf;
-    }
-
-    public int getIdProfessor() {
-        return idProfessor;
-    }
-
-    public void setIdProfessor(int idProfessor) {
-        this.idProfessor = idProfessor;
     }
 
     public String getNome() {
@@ -48,11 +39,11 @@ public class Professor extends SugarRecord {
         if (this == o) return true;
         if (!(o instanceof Professor)) return false;
         Professor professor = (Professor) o;
-        return getIdProfessor() == professor.getIdProfessor() && Objects.equals(getNome(), professor.getNome()) && Objects.equals(getCpf(), professor.getCpf());
+        return Objects.equals(getNome(), professor.getNome()) && Objects.equals(getCpf(), professor.getCpf());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getIdProfessor(), getNome(), getCpf());
+        return Objects.hash(getNome(), getCpf());
     }
 }
